f8cf89766c9aa442e28fc2a8ec0b03b111432cb7
Duran Audrey <aduran@bs406-s16-21.insa-lyon.fr>
2014-12-19 16:35:43 +0100

    Initialization

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "bwindow.h"
+#include "Agent.h"
+#include "Population.h"
+
+
+int main()
+{
+	srand(time(NULL));
+
+	Agent test;
+	Agent test2;
+    printf("x=%lf y=%lf vx=%lf \n", test.getx(), test.gety(), test.getVx());
+
+    Population pop;
+    pop.add(test);
+	
+    bwindow win(640,480);
+    printf("%d\n",win.init());
+    win.map();
+    for(;;)
+    {
+	int ev = win.parse_event();
+	switch(ev)
+	{
+	    case BKPRESS :
+		printf("keypressed\n"); 
+		printf("key : %s\n",win.get_lastkey());
+		break;
+	    case BBPRESS:
+		printf("buttonpressed\n"); break;
+	    case BEXPOSE:
+		printf("expose\n"); break;
+	    case BCONFIGURE:
+		printf("configure\n"); break;
+	}
+	win.draw_point(100,100,0xFF00);
+	win.draw_line(100,100,200,200,0xFF0000);
+	win.draw_text(10,10,0x0,"Yo men",strlen("Yo men"));
+	win.draw_square(200,200,220,220,0xFF00);
+	win.draw_fsquare(400,400,440,440,0xFF00);
+    }
+    
+
+
+    
+
+    return 0;
+}
---
+++ makefile
@@ -0,0 +1,23 @@
+
+all: test.out
+
+test.out: main.o bwindow.o Agent.o Population.o
+	g++ -o test.out main.o Agent.o Population.o bwindow.o -lX11 -L/usr/X11R6/lib
+
+main.o: main.cpp
+	g++ -c main.cpp 
+
+bwindow.o: bwindow.cpp bwindow.h
+	g++ -c bwindow.cpp
+
+Agent.o: Agent.cpp Agent.h
+	g++ -c Agent.cpp
+
+Population.o: Population.cpp Population.h
+	g++ -c Population.cpp
+	
+clean:
+	rm *.o *.out
+
+mrproper: clean
+	rm test.out
